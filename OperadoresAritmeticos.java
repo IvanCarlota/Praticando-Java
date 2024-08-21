26e7d2989aff93b8c55c5c0b1b0681b58fb4d5ec
Ivan Junior Carlota <ivan.junior.carlota@gmail.com>
2024-08-21 17:43:49 -0300

    Operadores aritméticos em Java

---
+++ OperadoresAritmeticos.java
@@ -0,0 +1,26 @@
+public class OperadoresAritmeticos { 
+    public static void main(String[] args) {
+
+       int minhaIdade = 40;
+       int suaIdade = 25;
+        // Adição
+       int totalIdades = minhaIdade + suaIdade;
+       System.out.println("A soma de Total das idades é: " + totalIdades);
+       
+       // Subtração
+       int subtracaoIdades = minhaIdade - suaIdade;
+       System.out.println("A divisão de minhaIdade e a suaIdade é: " + subtracaoIdades);
+       
+       // Multiplicação
+       int multiplicacaoIdades = minhaIdade * suaIdade;
+       System.out.println("A Multiplicação da minhaIdade * suaIdade é: " + multiplicacaoIdades);
+       
+       // Divisão
+       int divisaoIdades = minhaIdade / suaIdade;
+       
+       System.out.println("A Divisão da minhaIdade / suaIdade é: " + divisaoIdades);
+
+    }
+
+
+}
