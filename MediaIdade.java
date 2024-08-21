f34d929b6ae1fe49271052ecd658f7ce5864622f
Ivan Junior Carlota <ivan.junior.carlota@gmail.com>
2024-08-21 17:57:59 -0300

    Media em Java

---
+++ MediaIdade.java
@@ -0,0 +1,16 @@
+public class MediaIdade { 
+    public static void main(String[] args) {
+
+       int minhaIdade = 40;
+       int suaIdade = 25;
+       int joaoIdade = 45;
+
+       int totalIdades = (minhaIdade + suaIdade + joaoIdade) / 3;
+       System.out.println("A soma de Total das idades é: " + totalIdades);
+       
+   
+
+    }
+
+
+}
