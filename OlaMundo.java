17476245bf7f0ec65022d3d50e0137b267e0d478
Ivan Junior Carlota <ivan.junior.carlota@gmail.com>
2024-08-20 22:30:16 -0300

    Hello Wordl

---
+++ OlaMundo.java
@@ -0,0 +1,16 @@
+// Você pode comentar uma linha de código inteira
+public class OlaMundo { // Você pode comentar na frente de uma declaração ou linha de instrução
+    public static void main(String[] args) {
+
+        //Imprime um comentário na sáida padrão
+        System.out.println("Olá Mundo!"); 
+
+    }
+/*
+Comentário 
+de 
+várias 
+linhas
+*/
+
+}
