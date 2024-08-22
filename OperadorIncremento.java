a16cfcbca77c6ef93ae4fd76757b35b4a4f43753
Ivan Junior Carlota <ivan.junior.carlota@gmail.com>
2024-08-22 14:57:05 -0300

    Utilizando o incremento ++ antes da variável para adicionar um valor e atribuílo

---
+++ OperadorIncremento.java
@@ -0,0 +1,11 @@
+public class OperadorIncremento { 
+    public static void main(String[] args) {
+
+       int totalDonloads = 40;
+       System.out.println("A variável totalDonloads posseui o valor : " + totalDonloads);
+       System.out.println("Incrementando + 1 à variável totalDonloads passa para : " + ++totalDonloads);
+
+    }
+
+
+}
