6f14ddb3d4cb9b4151080bb18d14e0739276d63e
Ivan Junior Carlota <ivan.junior.carlota@gmail.com>
2024-08-22 14:50:16 -0300

    Abreviando Operadores Aritmeticos em Java

---
+++ AbreviandoOperadoresAritmeticos.java
@@ -0,0 +1,18 @@
+public class AbreviandoOperadoresAritmeticos { 
+    public static void main(String[] args) {
+
+       int quantidadeProdutos = 40;
+
+       // Isso aqui:
+       // quantidadeProdutos = quantidadeProdutos + 5;
+
+       // É o mesmo que isso aqui
+       quantidadeProdutos += 5;
+
+      
+       
+       System.out.println(quantidadeProdutos);
+    }
+
+
+}
