037f91d767d05dd53d013c1b1213e4ad079d6866
Ivan Junior Carlota <ivan.junior.carlota@gmail.com>
2024-08-28 20:27:25 -0300

    Precendência de operadores lógicos

---
+++ 02 - Estrutura de controle e Operadores/PrecendenciaOperadoresLogicos.java
@@ -0,0 +1,11 @@
+public class PrecendenciaOperadoresLogicos {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+
+        boolean resultado = x == 13 && y == 15 || y == 20;
+
+        System.out.println(resultado);
+
+    }    
+}
